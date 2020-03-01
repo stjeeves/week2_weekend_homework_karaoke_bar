8d38c07d7d3932ba8908baccf02dacbdf958573d
Stephen <st-jeeves@hotmail.co.uk>
2020-03-01 21:12:00 +0000

    MVP done.

--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -12,7 +12,10 @@ class TestRoom < MiniTest::Test
   def setup()
 
     @room1 = Room.new("The Whitney Houston Suite", 4,
-      [@song1, @song2, @song3])
+      [@song1, @song2, @song3], 50)
+
+    @room2 = Room.new("The Bon Jovi Suite", 3, [@song1, @song2, @song3],
+    [@guest2, @guest3, @guest4], 50)
 
     @song1 = Song.new("Bohemian Rhapsody", "Queen")
     @song2 = Song.new("Ring Of Fire", "Johnny Cash")
@@ -25,7 +28,7 @@ class TestRoom < MiniTest::Test
     @guest2 = Guest.new("Matthew", 200, "Bohemian Rhapsody")
     @guest3 = Guest.new("Michael", 150, "Ring Of Fire")
     @guest4 = Guest.new("Ariane", 100, "K-Pop Nonsense")
-    @guest5 = Guest.new("Bjorn", 500, "Foundations")
+    @guest5 = Guest.new("Bjorn", 500, "Whats My Age Again?")
 
     @song_list = [@song1, @song2, @song3, @song4, @song5]
 
@@ -48,9 +51,9 @@ class TestRoom < MiniTest::Test
     assert_equal(4, @room1.add_song_to_playlist(@song6))
   end
 
-  def test_remove_song_from_playlist()
-    assert_equal(2, @room1.remove_song_from_playlist(@song1))
-  end
+  # def test_remove_song_from_playlist()
+  #   assert_equal(2, @room1.remove_song_from_playlist(@song1))
+  # end
 
   def test_check_in_to_room()
     assert_equal(1, @room1.check_in_to_room(@guest1))
@@ -64,5 +67,30 @@ class TestRoom < MiniTest::Test
   def test_can_guest_check_in_true()
     assert_equal(true, @room1.can_guest_check_in())
   end
+
+  def test_can_guest_check_in_false()
+    assert_equal(false, @room2.can_guest_check_in())
+  end
+
+
+  def test_guest_pay_for_room()
+    assert_equal(70, @room1.guest_pay_for_room(@guest1))
+  end
+
+  def test_check_playlist_for_favourite_song_true()
+    assert_equal("Woo hoo!", @room1.check_playlist_for_favourite_song(@guest1))
+  end
+
+  def test_check_playlist_for_favourite_song_false()
+    assert_equal("Boo!", @room1.check_playlist_for_favourite_song(@guest5))
+  end
+
+  # def test_whole_proccess_space()
+  #   assert_equal("Yaldy!", @room1.whole_proccess(@guest1))
+  # end
+  #
+  # def test_whole_proccess_no_space()
+  #   assert_equal("No the night pal", @room2.whole_proccess(@guest1))
+  # end
 
 end
